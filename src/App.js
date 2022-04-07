d4acc6aea8a0fe2f87a069cae138bc61e79d1c6b
El houssaïne El jadid <ikky57@gmail.com>
2022-04-07 10:50:00 +0200

    ajout de fonction dans membres

--- src/App.js
+++ src/App.js
@@ -26,23 +26,49 @@ const famille = {
   },
 }
 class App extends Component {
-  state = { famille }
+  state = { famille, isShow: false }
   handleClick = (param) => {
     const famille = { ...this.state.famille }
     famille.membre1.age += param
     this.setState({ famille })
   }
+  handleChange = (e) => {
+    const famille = { ...this.state.famille }
+    const name = e.target.value
+    famille.membre1.name = name
+    this.setState({ famille })
+  }
+
+  handleShowDescription = () => {
+    const isShow = !this.state.isShow
+    this.setState({ isShow })
+
+
+
+  }
+
+
 
   render() {
     const { titre } = this.props;//destructuration
-    // const { famille } = this.state
+    const { famille, isShow } = this.state
+
     return (
       <div className="App">
         <h1>{titre}</h1> {/*this.props.titre devient titre suite a la destructuration*/}
         <input type="texte" value={famille.membre1.name} onChange={this.handleChange}></input>
         <Membre
           name={famille.membre1.name}
-          age={famille.membre1.age} />description lol
+          age={famille.membre1.age} />
+        {
+          isShow ? <strong>description lol</strong> : null
+        }
+        <button onClick={this.handleShowDescription}>
+          {
+            isShow ? 'cacher' : 'Montrer'
+          }
+        </button>
+
         <Membre
           name={famille.membre2.name}
           age={famille.membre2.age} />
--- src/Membre.js
+++ src/Membre.js
@@ -7,8 +7,9 @@ const Membre = ({ name, children, age }) => {
 
     return (
         <fragment>
-            <h2>Membre de ma famille : {name.toUpperCase()} : {age} </h2>
+            <h2 style={{ backgroundColor: age < 26 ? 'darkblue' : 'yellow', color: age < 26 ? 'lightgreen' : 'black' }}>Membre de ma famille : {name.toUpperCase()} : {age} </h2>
             {children ? <p>description: {children}</p> : <Fragment />}
+
         </fragment>
     )
 }
